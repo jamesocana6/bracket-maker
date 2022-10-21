13922c23145d9676a63177dca42ff396a2075b93
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-10-21 14:36:27 -0400

    added view to tournaments index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("tournaments/", views.tournaments_index, name="tournaments_index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ def home(request):
 
 def about(request):
     return render(request, "about.html")
+
+def tournaments_index(request):
+    return render(request, "tournaments/index.html")
