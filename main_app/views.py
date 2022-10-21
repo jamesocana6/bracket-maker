ce43ce7a89595bb415e20a2307c5a31ea93720b1
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-10-21 14:54:17 -0400

    mock tournament data posts on index page

--- main_app/views.py
+++ main_app/views.py
@@ -6,5 +6,20 @@ def home(request):
 def about(request):
     return render(request, "about.html")
 
+class Tournament():
+    def __init__(self, name, venue, players, prize_pool, is_complete = False):
+        self.name = name
+        self.venue = venue
+        self.players = players
+        self.prize_pool = prize_pool
+        self.is_complete = is_complete
+
+tournaments = [
+    Tournament("The Big House 4", "University of Michigan", ["Mang0", "Plup", "Lucky", "Hungrybox", "Mew2King"], 20000),
+    Tournament("Smash Summit 9", "Summit House", ["Mang0", "Plup", "Zain", "Hungrybox", "Amsa"], 40000, True),
+    Tournament("RPS", "Zoom Call", ["James", "Kyler", "Peter", "Drew", "Sylvia"], 40000, True),
+]
+        
+
 def tournaments_index(request):
-    return render(request, "tournaments/index.html")
+    return render(request, "tournaments/index.html", {"tournaments" : tournaments})
